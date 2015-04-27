2872f02359a75eb3abc7a7bfd852fd2822af9311
jacethelefty <jacemontoya@live.com>
2015-04-27 10:04:50 -0700

    share slider fix

--- js/app.js
+++ js/app.js
@@ -23,14 +23,8 @@ $('#socBox').on('click', function(){
   }
 });
 
-$(document).ready (function() {
-  $('.icon-share2').on('click', function(){
-  $(this).removeClass('icon-share2');
-  $(this).addClass('icon-cross');
-  $('.icon-cross').on('click', function(){
-  $(this).removeClass('icon-cross');
-  $(this).addClass('icon-share2');
-});
+$( ".icon-share2" ).click(function() {
+  $( this ).toggleClass( "icon-share2 icon-cross" );
 });
 
 $(document).ready (function() {
@@ -40,7 +34,6 @@ $( "#fig" ).tooltip({ content: '<p class="tool">The juicier the better!</p><img 
 $( "#lemon" ).tooltip({ content: '<img src="./imgs/lemon.jpg" />' });
 $( "#cream" ).tooltip({ content: '<img src="./imgs/cream.jpg" />' });
 $( "#roughChop" ).tooltip({ content: '<p class="tool">You can substitute other kinds of roasted nuts!</p><img src="./imgs/pecans.jpg" />' });
-});
 });
 
 $(function() {
